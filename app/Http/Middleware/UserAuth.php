fabd8be3fd831b8a54423ef26858f74653cd5e35
RomitWorkSpace <romitkumar848@gmail.com>
2023-09-23 17:31:30 +0530

    To Check User Authentication

---
+++ app/Http/Middleware/UserAuth.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UserAuth
+ {
+    public function handle(Request $request, Closure $next)
+    {
+        if(!session()->has('LoggedUser') && $request->path()!='/login'){
+          return redirect('/login')->with('fail','You must be logged in');
+        }
+        if(session()->has('LoggedUser') && $request->path()=='/login'){
+          return back();
+        }
+        $response=$next($request);
+        $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma','no-cache');
+        $response->headers->set('Expires','Sat, 01 Jan 2020 00:00:00 GMT');
+        return $response;
+    }
+ }
