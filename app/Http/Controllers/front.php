66068545e0364be84141394961b4237d2ac69ca6
RomitWorkSpace <romitkumar848@gmail.com>
2023-09-27 01:26:00 +0530

    Login and Logout Function

--- app/Http/Controllers/front.php
+++ app/Http/Controllers/front.php
@@ -18,9 +18,45 @@ class front extends Controller
 
     public function phone_price(Request $request){
          $data = $request;
+         $user=$data->username;
+         $detail=DB::table('customers')->where('username',$user)->first();
+        
+         return view('front/phone_price', ['data' => $data,'detail' => $detail]);
     }
     public function user_login(){
         return view('front/user_login');
     }
+
+    public function price_generate(){
+      return view('front/phone_price');
+    }
+
+    public function login(Request $request){
+        $request->validate([
+           'username'=>'required',
+           'password'=>'required'
+          ]);
+          
+          $user = DB::table('customers')->where('username',$request->username)->first();
+          
+          if($user){
+            $password = sha1($request->password);
+            if($user->password == $password){
+              $request->session()->put('LoggedUser',$user->username);
+              return redirect('/sell-phone/smart-phone');
+            }else{
+              return redirect()->back()->with('fail','Wrong Password! Try again');
+            }
+          }else{
+            return redirect()->back()->with('fail','Email not matched! Try again');
+          }
+      }
+      
+      public function logout(){
+         if(session()->has('LoggedUser')){
+            session()->pull('LoggedUser');
+            return redirect('/');
+         }
+      } 
 
 }
